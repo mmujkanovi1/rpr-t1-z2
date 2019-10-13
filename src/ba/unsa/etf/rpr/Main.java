0c2b7f7de48f69bdee9b890a5a20db58ffdcf274
Mujkanovic Mirza <mmujkanovi1@etf.unsa.ba>
2019-10-13 13:54:48 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int sumaCifara(int n) {
+        int suma=0;
+        while(n!=0) {
+            suma+=n%10;
+            n/=10;
+        }
+        return suma;
+
+
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner ulaz=new Scanner(System.in);
+        int n=ulaz.nextInt();
+        int s=0;
+        for(int i=1;i<=n;i++) {
+            if(i%sumaCifara(i)==0) System.out.println(i);
+
+        }
+
+
+
+    }
+}
